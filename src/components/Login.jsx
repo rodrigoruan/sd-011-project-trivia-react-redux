f277c6164bb705b48247c80262cb54fb7e5b0ff0
Samuel Melo <samuel.silva.melo@hotmail.com>
2021-08-22 20:43:06 -0300

    Add email validation

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -26,7 +26,13 @@ class Login extends React.Component {
       },
       () => {
         const { userName, email } = this.state;
-        if (userName && email) {
+        // REGEX VALIDATION REFERENCES
+        // https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
+        // https://pt.stackoverflow.com/questions/1386/express%C3%A3o-regular-para-valida%C3%A7%C3%A3o-de-e-mail
+        // https://html.spec.whatwg.org/multipage/input.html#valid-e-mail-address
+        const emailRegex = /[a-z0-9._-]+@[a-z0-9]+\.[a-z]+$/i;
+        const checkEmail = emailRegex.test(email.toLocaleLowerCase());
+        if (userName && checkEmail) {
           this.setState({
             disabled: false,
           });
